f12e9d771af246a843850538b3fdd2466709f950
mdtalha109 <mdtalha1041@gmail.com>
2023-12-10 00:22:35 +0530

    feat: board creation
    added board creation form and basic list of board

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,27 @@
+
+import { Button } from "@/components/ui/button"
+import { db } from "@/lib/db"
+import { Board } from "./board"
+import { Form } from "./form"
+
+const OrganizationIdPage = async() => {
+
+    const boards = await db.board.findMany()
+    console.log("boards: ", boards)
+
+   
+    return (
+        <div className="flex flex-col space-y-4">
+            <Form/>
+            <div className="space-y-2">
+                {
+                    boards?.map((board) => (
+                        <Board key={board.id} title={board.title} id={board.id}/>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default OrganizationIdPage
