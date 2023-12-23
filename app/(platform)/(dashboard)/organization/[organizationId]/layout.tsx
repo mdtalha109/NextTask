dcff26885226a795dec2f58ee6c3dab11e760e16
mdtalha109 <mdtalha1041@gmail.com>
2023-12-23 23:35:47 +0530

    feat: board & creating new list

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -2,13 +2,15 @@
 import { auth } from "@clerk/nextjs";
 import { OrgControl } from "./_components/org-control";
 
+import { startCase } from 'lodash'
+
 
 
 export async function generateMetadata() {
   const { orgSlug } = auth();
 
   return {
-    // title: startCase(orgSlug || "organization"),
+    title: startCase(orgSlug || "organization"),
   };
 };
 
