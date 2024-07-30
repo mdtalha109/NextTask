35f528044dbc83a76932fe135447edd530318b5d
mdtalha109 <mdtalha1041@gmail.com>
2024-07-31 00:44:20 +0530

    style: fixed styling issue for mobile screen

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -7,16 +7,12 @@ export const Navbar = () => {
   return (
     <div className="fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
-        NextTask 
-        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button size="sm" variant="outline" asChild>
+        <div>NextTask</div> 
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between">
+          
+          <Button size="sm" asChild>
             <Link href="/sign-in">
               Login
-            </Link>
-          </Button>
-          <Button size="sm" asChild>
-            <Link href="/sign-up">
-              Get NextTask for free
             </Link>
           </Button>
         </div>
